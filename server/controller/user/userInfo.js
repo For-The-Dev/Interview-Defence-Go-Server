fcf6006af100f10b450fd1048c3181d76eb78ff4
billy5982 <kuu0404@naver.com>
2023-04-09 23:28:00 +0900

    유저 정보 추출 메서드 완료

--- server/controller/user/userInfo.js
+++ server/controller/user/userInfo.js
@@ -1,4 +1,25 @@
 require('dotenv').config();
 const axios = require('axios');
+const getUserInfo = require('../../utils/getUserInfo');
+const models = require('../../models');
 
-module.exports = async (req, res) => {};
+module.exports = async (req, res) => {
+  const { authorization } = req.headers;
+  if (!authorization) return res.status(401).send('Invalid authorization');
+
+  try {
+    const { userId, err } = await getUserInfo(authorization);
+    // 잘못된 토큰을 사용했을 경우 undefined의 값이 출력됨
+    if (err) return res.status(401).send('Invalid user');
+    const user = await models.Users.findOne({
+      where: {
+        userId,
+      },
+    });
+
+    return res.status(200).send({ nickName: user.nickName, avatar_url: user.avatar_url });
+  } catch (e) {
+    console.log(e);
+    return res.send(e);
+  }
+};
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,29 +1,9 @@
 const express = require('express');
 const models = require('../models');
-
+const controller = require('../controller');
 const router = express.Router();
 
-// router.post('/register', async (req, res) => {
-//   const { userName } = req.body;
-
-//   try {
-//     // 회원가입 된 유저가 가져야할 항목 설정
-//     const userModel = models.sequelize.define(userName, {
-//       userId: {
-//         type: models.Sequelize.STRING(20),
-//         primaryKey: true,
-//       },
-//     });
-
-//     // 유저 이름에 데이블 생성
-//     await models.sequelize.sync();
-
-//     res.send('create');
-//   } catch (e) {
-//     console.log('err');
-//     console.log(e);
-//     res.status(401).send(e);
-//   }
-// });
+// 유저 정보 조회
+router.get('/', controller.userInfo);
 
 module.exports = router;
