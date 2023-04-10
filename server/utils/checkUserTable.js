fb9979e80119952a5ac75fde93b4ce772edde8e7
billy5982 <kuu0404@naver.com>
2023-04-10 21:21:06 +0900

    github 유저 확인 함수 제작

---
+++ server/utils/checkUserTable.js
@@ -0,0 +1,18 @@
+const getUserInfo = require('./getUserInfo');
+const models = require('../models');
+
+module.exports = async (accessToken) => {
+  try {
+    const { userId } = await getUserInfo(accessToken);
+
+    const { userId: findId } = await models.Users.findOne({
+      where: {
+        userId,
+      },
+    });
+    // 유저 아이디를 반환함
+    return findId;
+  } catch (e) {
+    return new Error(e);
+  }
+};
