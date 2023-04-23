61f9d007358edc984d9a26e8d89e74981b4148f1
billy5982 <kuu0404@naver.com>
2023-04-23 19:01:44 +0900

    api 요청방법 변경 : body -> query parameter, 최소 질문 개수 수정 : 5개, ai 데이터 처리 방식 변경 : 병렬처리 방식

--- server/controller/getGpt/interviewSet.ts
+++ server/controller/getGpt/interviewSet.ts
@@ -10,32 +10,55 @@ const configuration = new Configuration({
 
 const openAi = new OpenAIApi(configuration);
 
+const getAiInterview = (stack: string) => {
+  return openAi.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `면접자와의 인터뷰를 위해 ${stack}를 사용하는 기업에서 나올 수 있는 기술 면접 1가지를 면접관 입장에서 질문해주세요.
+    질문에 끝맺음은 '설명해주세요.'로 끝내주세요.`,
+
+    // messages: [{ role: 'user', content: `${question}` }],
+    max_tokens: 200,
+    // 다음 단어의 예측 불확실성을 제어하는 옵션임. 값이 높을수록 모델은 예측을 더 불확실한 방향으로 수행함.
+    temperature: 0.5,
+    // 모델이 생성할 수 있는 다음 단어 후보를 제한하는 옵션. 0.9라면 다음 단어 후보중에서 확률 분포의 상위 90%에 해당 하는 단어만 고려함.
+    top_p: 1.0,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
+  });
+};
+
 const interviewSet = async (req: Request, res: Response) => {
-  const { stack } = req.body;
-  if (!stack) return res.status(400).send('메세지를 입력해주세요');
+  let stacks = req.query.stacks;
+  stacks = stacks?.toString();
+  if (!stacks) return res.status(400).send('메세지를 입력해주세요');
+
+  let stacksArr = stacks.split(',');
+
+  // 질문은 최소 5가지 이상 나오도록 설정
+  if (stacksArr.length < 5) {
+    const leng = stacksArr.length;
+    for (let i = leng; i < 5; i++) {
+      let randomIdx = Math.floor(Math.random() * leng);
+      stacksArr.push(stacksArr[randomIdx]);
+    }
+  }
 
   try {
-    const response = await openAi.createCompletion({
-      model: 'text-davinci-003',
-      prompt: `면접자와의 인터뷰를 위해 ${stack}를 사용하는 기업에서 나올 수 있는 기술 면접 5가지를 면접관 입장에서 질문해주세요.
-      단, max_tokens가 1000이 넘지 않게 해주세요.
-      질문에 끝맺음은 '설명해주세요.'로 끝내주세요.`,
-      // messages: [{ role: 'user', content: `${question}` }],
-      max_tokens: 1000,
-      // 다음 단어의 예측 불확실성을 제어하는 옵션임. 값이 높을수록 모델은 예측을 더 불확실한 방향으로 수행함.
-      temperature: 0.5,
-      // 모델이 생성할 수 있는 다음 단어 후보를 제한하는 옵션. 0.9라면 다음 단어 후보중에서 확률 분포의 상위 90%에 해당 하는 단어만 고려함.
-      top_p: 1.0,
-      frequency_penalty: 0.0,
-      presence_penalty: 0.0,
+    const responses = await Promise.all(
+      stacksArr.map((stack: string) => {
+        return getAiInterview(stack);
+      })
+    );
+
+    const interviewDataFilter = responses.map((data) => {
+      return data.data.choices[0].text
+        .split(/[\n!@#$%^&*(),.?":{}|<>]/)
+        .join('');
     });
 
-    const data = response.data.choices[0].text
-      .split('\n')
-      .filter((question: string) => question.length > 0);
-
-    res.json(data);
+    res.json(interviewDataFilter);
   } catch (e) {
+    console.log(e);
     res.status(500).send('서버 에러');
   }
 
