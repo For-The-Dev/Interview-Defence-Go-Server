0ec3b395100c5207f265c732258e04fc4c6bba86
billy5982 <kuu0404@naver.com>
2023-05-10 23:24:29 +0900

    🐛  fix : interview 응답에 대해 끝맺음을 정확하게 할 수 있도록 수정

--- server/controller/getGpt/checkAllAnswer.ts
+++ server/controller/getGpt/checkAllAnswer.ts
@@ -17,11 +17,9 @@ interface QAType {
 const getAiData = (question: string, answer: string) => {
   return openAi.createCompletion({
     model: 'text-davinci-003',
-    prompt: `저는 매우 지능적인 질문 답변 로봇입니다. 만약 사용자가 기술 면접에서 ${question}에 대해 ${answer}이라고 답했다면,
-    해당 질문에 대한 답변 및 추가 설명을 해주세요.
-    단, max_tokens가 500이 넘지 않게 해주세요`,
+    prompt: `저는 max-tokens 350이내의 3문장으로 요약해서 답변하는 매우 지능적인 질문 답변 로봇입니다. 만약 사용자가 기술 면접에서 ${question}에 대해 ${answer}이라고 답했다면, 해당 질문에 대한 답변 및 추가 설명을 해주세요.`,
     // messages: [{ role: 'user', content: `${question}` }],
-    max_tokens: 500,
+    max_tokens: 350,
     // 다음 단어의 예측 불확실성을 제어하는 옵션임. 값이 높을수록 모델은 예측을 더 불확실한 방향으로 수행함.
     temperature: 0.2,
     // 모델이 생성할 수 있는 다음 단어 후보를 제한하는 옵션. 0.9라면 다음 단어 후보중에서 확률 분포의 상위 90%에 해당 하는 단어만 고려함.
--- server/controller/getGpt/editAnswer.ts
+++ server/controller/getGpt/editAnswer.ts
@@ -16,9 +16,7 @@ const editAnswer = async (req: Request, res: Response) => {
   try {
     const response = await openAi.createCompletion({
       model: 'text-davinci-003',
-      prompt: `저는 매우 지능적인 질문 답변 로봇입니다. 만약 사용자가 기술 면접에서 ${question}에 대해 ${answer}이라고 답했다면,
-      해당 질문에 대한 답변 및 추가 설명을 해주세요.
-      단, max_tokens가 500이 넘지 않게 해주세요`,
+      prompt: `저는 max-tokens 350이내의 3문장으로 요약해서 답변하는 매우 지능적인 질문 답변 로봇입니다. 만약 사용자가 기술 면접에서 ${question}에 대해 ${answer}이라고 답했다면, 해당 질문에 대한 답변 및 추가 설명을 해주세요.`,
       // messages: [{ role: 'user', content: `${question}` }],
       max_tokens: 500,
       // 다음 단어의 예측 불확실성을 제어하는 옵션임. 값이 높을수록 모델은 예측을 더 불확실한 방향으로 수행함.
